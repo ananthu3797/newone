7eaa51f1c9a5f2ec65b81de9f4bee08db37a40ca
ananthu3797 <ananthuloyola@gmail.com>
2020-11-02 14:40:26 +0530

    Adding

---
+++ basic example/Module1.vb
@@ -0,0 +1,20 @@
+﻿Using system;
+    Using System.Collections.Generic;
+    Using System.Linq;
+    Using System.Text
+Using System.Threading.Tasks;
+    Namespace basiceg
+    { 
+    
+    Class Helloworld
+        {
+        0 references
+            Static void Main()
+            {
+            Console.Write("Hello World")
+            Console.Read():
+        }
+            }
+            }
+    End Class
+End Namespace
